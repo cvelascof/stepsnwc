638df7b729e35ced22bab77070a778b7dcae486d
Seppo Pulkkinen <pulkkins@gmail.com>
2018-06-14 10:22:45 +0200

    Implement framework for generating nowcast ensembles

--- nowcast_generators.py
+++ nowcast_generators.py
@@ -88,99 +88,14 @@ def s_prog(R, V, num_timesteps, num_cascade_levels, R_thr, extrap_method,
       Three-dimensional array of shape (num_timesteps,m,n) containing a time 
       series of nowcast precipitation fields.
     """
-    _check_inputs(R, V, 2)
-    
-    if np.any(~np.isfinite(R)):
-        raise ValueError("R contains non-finite values")
-    
-    L = R.shape[1]
-    extrap_method = advection.get_method(extrap_method)
-    R = R.copy()
-    
-    # Advect the previous precipitation fields to the same position with the 
-    # most recent one (i.e. transform them into the Lagrangian coordinates).
-    R[0, :, :] = extrap_method(R[0, :, :], V, 2, outval="min", **extrap_kwargs)[1]
-    R[1, :, :] = extrap_method(R[1, :, :], V, 1, outval="min", **extrap_kwargs)[0]
-    
-    if conditional:
-        MASK = np.logical_and.reduce([R[i, :, :] >= R_thr for i in xrange(R.shape[0])])
-    else:
-        MASK = None
-    
-    # Initialize the band-pass filter.
-    filter_method = bandpass_filters.get_method(bandpass_filter_method)
-    filter = filter_method(L, num_cascade_levels, **filter_kwargs)
-    
-    # Compute the cascade decompositions of the input precipitation fields.
-    decomp_method = decomposition.get_method(decomp_method)
-    R_d = []
-    for i in xrange(3):
-        R_ = decomp_method(R[i, :, :], filter, MASK=MASK)
-        R_d.append(R_)
-    
-    # Normalize the cascades and rearrange them into a four-dimensional array 
-    # of shape (num_cascade_levels,3,L,L) for the AR(2) model.
-    R_c,mu,sigma = _stack_cascades(R_d, num_cascade_levels)
-    
-    # Compute lag-1 and lag-2 temporal autocorrelation coefficients for each 
-    # cascade level.
-    GAMMA = np.empty((num_cascade_levels, 2))
-    for i in xrange(num_cascade_levels):
-        R_c_ = np.stack([R_c[i, j, :, :] for j in xrange(3)])
-        GAMMA[i, :] = correlation.temporal_autocorrelation(R_c_, MASK=MASK)
-    
-    _print_corrcoefs(GAMMA)
-    
-    # Adjust the lag-2 correlation coefficient to ensure that the AR(2) process 
-    # is stationary.
-    for i in xrange(num_cascade_levels):
-        GAMMA[i, 1] = autoregression.adjust_lag2_corrcoef(GAMMA[i, 0], GAMMA[i, 1])
-    
-    # Estimate the parameters of the AR(2) model from the autocorrelation 
-    # coefficients.
-    PHI = np.empty((num_cascade_levels, 2))
-    for i in xrange(num_cascade_levels):
-        PHI[i, :] = autoregression.estimate_ar_params_yw(GAMMA[i, :])
-    
-    _print_ar2_params(PHI, False)
-    
-    # Discard the first of the three cascades because it is not needed for the 
-    # AR(2) model.
-    R_c = R_c[:, 1:, :, :]
-    
-    D = None
-    R_f = []
-    
-    #MASK_p = (R[-1, :, :] >= R_thr).astype(float) # precipitation mask
-    #R_min = np.min(R)
-    
-    for t in xrange(num_timesteps):
-        # Iterate the AR(2) model for each cascade level.
-        for i in xrange(num_cascade_levels):
-            R_c[i, :, :, :] = autoregression.iterate_ar_model(R_c[i, :, :, :], 
-                                                              PHI[i, :])
-        
-        # Compute the recomposed precipitation field from the cascade obtained 
-        # from the AR(2) model.
-        R_r = [(R_c[i, -1, :, :] * sigma[i]) + mu[i] for i in xrange(num_cascade_levels)]
-        R_r = np.sum(np.stack(R_r), axis=0)
-        
-        # Advect the recomposed precipitation field to obtain the forecast for 
-        # time step t.
-        R_f_,D = advection.semilagrangian(R_r, V, 1, D_prev=D, return_displacement=True)
-        R_f_ = R_f_[0]
-        
-        # Advect the precipitation mask and apply it to the output.
-        # TODO: Make the precipitation mask optional.
-        #MASK_p_ = advection.semilagrangian(MASK_p, V, 1, D_prev=D)
-        #R_f_[MASK_p < 0.5] = R_min
-        
-        R_f.append(R_f_)
-    
-    return R_f
+    return _steps(R, V, num_timesteps, num_cascade_levels, R_thr, extrap_method, 
+                  decomp_method, bandpass_filter_method, False, 
+                  conditional=conditional, extrap_kwargs=extrap_kwargs, 
+                  filter_kwargs=filter_kwargs)
 
-def steps(R, V, num_timesteps, extrap_method, num_ens_members, 
-          num_cascade_levels, extrap_args={}, ar_order=2):
+def steps(R, V, num_timesteps, num_ens_members, num_cascade_levels, R_thr, 
+          extrap_method, decomp_method, bandpass_filter_method, vp_par, vp_perp, 
+          conditional=True, extrap_kwargs={}, filter_kwargs={}):
     """Generate a nowcast ensemble by using the STEPS method (Bowler et al., 2006).
     
     Parameters
@@ -188,25 +103,46 @@ def steps(R, V, num_timesteps, extrap_method, num_ens_members,
     R : array-like
       Three-dimensional array of shape (3,m,n) containing three input 
       precipitation fields ordered by timestamp from oldest to newest. The time 
-      steps between the inputs are assumed to be regular.
+      steps between the inputs are assumed to be regular, and the inputs are 
+      required to have finite values.
     V : array-like
       Array of shape (2,m,n) containing the x- and y-components of the advection 
       field. The velocities are assumed to represent one time step between the 
       inputs.
     num_timesteps : int
       Number of time steps to forecast.
+    num_ens_members : int
+      The number of ensemble members to generate.
+    num_cascade_levels : int
+      The number of cascade levels to use.
+    R_thr : float
+      Specifies the threshold value to use if conditional is True.
     extrap_method : str
       Name of the extrapolation method to use. See the documentation of the 
       advection module for the available choices.
-    num_ens_members : int
-      Number of ensemble members to generate.
-    num_cascade_levels : int
-      Number of cascade levels to use.
-    extrap_args : dict
+    decomp_method : str
+      Name of the cascade decomposition method to use, see the documentation 
+      of the cascade.decomposition module.
+    bandpass_filter_method : str
+      Name of the bandpass filter method to use with the cascade decomposition, 
+      see the documentation of the cascade.bandpass_filters module.
+    vp_par : tuple
+      Three-element tuple containing the parameters for the standard deviation 
+      of the perturbations in the direction parallel to the motion vectors. 
+      See perturbation.motion_generators.initialize_motion_perturbations_bps.
+    vp_perp : tuple
+      Three-element tuple containing the parameters for the standard deviation 
+      of the perturbations in the direction perpendicular to the motion vectors. 
+      See perturbation.motion_generators.initialize_motion_perturbations_bps.
+    conditional : bool
+      If set to True, compute the correlation coefficients conditionally by 
+      excluding the areas where the values are below the threshold R_thr.
+    extrap_kwargs : dict
       Optional dictionary that is supplied as keyword arguments to the 
       extrapolation method.
-    ar_order : int
-      Optional parameter for the order of the autoregressive model to use.
+    filter_kwargs : dict
+      Optional dictionary that is supplied as keyword arguments to the 
+      filter method.
     
     Returns
     -------
@@ -215,7 +151,11 @@ def steps(R, V, num_timesteps, extrap_method, num_ens_members,
       containing a time series of forecast precipitation fields for each ensemble 
       member.
     """
-    pass
+    return _steps(R, V, num_timesteps, num_cascade_levels, R_thr, 
+                  extrap_method, decomp_method, bandpass_filter_method, True, 
+                  num_ens_members=num_ens_members, conditional=conditional, 
+                  extrap_kwargs=extrap_kwargs, filter_kwargs=filter_kwargs, 
+                  vp_par=vp_par, vp_perp=vp_perp)
 
 def _check_inputs(R, V, method):
   if method == 1:
@@ -285,5 +225,113 @@ def _stack_cascades(R_d, num_levels):
   
   return np.stack(R_c),mu,sigma
 
-#def _steps():
-#  pass
+def _steps(R, V, num_timesteps, num_cascade_levels, R_thr, extrap_method, 
+           decomp_method, bandpass_filter_method, add_perturbations, 
+           num_ens_members=1, conditional=True, extrap_kwargs={}, 
+           filter_kwargs={}, vp_par=None, vp_perp=None):
+    _check_inputs(R, V, 2)
+    
+    if np.any(~np.isfinite(R)):
+        raise ValueError("R contains non-finite values")
+    
+    L = R.shape[1]
+    extrap_method = advection.get_method(extrap_method)
+    R = R.copy()
+    
+    # Advect the previous precipitation fields to the same position with the 
+    # most recent one (i.e. transform them into the Lagrangian coordinates).
+    R[0, :, :] = extrap_method(R[0, :, :], V, 2, outval="min", **extrap_kwargs)[1]
+    R[1, :, :] = extrap_method(R[1, :, :], V, 1, outval="min", **extrap_kwargs)[0]
+    
+    if conditional:
+        MASK = np.logical_and.reduce([R[i, :, :] >= R_thr for i in xrange(R.shape[0])])
+    else:
+        MASK = None
+    
+    # Initialize the band-pass filter.
+    filter_method = bandpass_filters.get_method(bandpass_filter_method)
+    filter = filter_method(L, num_cascade_levels, **filter_kwargs)
+    
+    # Compute the cascade decompositions of the input precipitation fields.
+    decomp_method = decomposition.get_method(decomp_method)
+    R_d = []
+    for i in xrange(3):
+        R_ = decomp_method(R[i, :, :], filter, MASK=MASK)
+        R_d.append(R_)
+    
+    # Normalize the cascades and rearrange them into a four-dimensional array 
+    # of shape (num_cascade_levels,3,L,L) for the AR(2) model.
+    R_c,mu,sigma = _stack_cascades(R_d, num_cascade_levels)
+    
+    # Compute lag-1 and lag-2 temporal autocorrelation coefficients for each 
+    # cascade level.
+    GAMMA = np.empty((num_cascade_levels, 2))
+    for i in xrange(num_cascade_levels):
+        R_c_ = np.stack([R_c[i, j, :, :] for j in xrange(3)])
+        GAMMA[i, :] = correlation.temporal_autocorrelation(R_c_, MASK=MASK)
+    
+    _print_corrcoefs(GAMMA)
+    
+    # Adjust the lag-2 correlation coefficient to ensure that the AR(2) process 
+    # is stationary.
+    for i in xrange(num_cascade_levels):
+        GAMMA[i, 1] = autoregression.adjust_lag2_corrcoef(GAMMA[i, 0], GAMMA[i, 1])
+    
+    # Estimate the parameters of the AR(2) model from the autocorrelation 
+    # coefficients.
+    PHI = np.empty((num_cascade_levels, 2))
+    for i in xrange(num_cascade_levels):
+        PHI[i, :] = autoregression.estimate_ar_params_yw(GAMMA[i, :])
+    
+    _print_ar2_params(PHI, False)
+    
+    # Discard the first of the three cascades because it is not needed for the 
+    # AR(2) model.
+    R_c = R_c[:, 1:, :, :]
+    
+    # Stack the cascades into a five-dimensional array containing all ensemble 
+    # members.
+    R_c = np.stack([R_c.copy() for i in xrange(num_ens_members)])
+    
+    D = [None for j in xrange(num_ens_members)]
+    R_f = [[] for j in xrange(num_ens_members)]
+    
+    #MASK_p = (R[-1, :, :] >= R_thr).astype(float) # precipitation mask
+    #R_min = np.min(R)
+    
+    for t in xrange(num_timesteps):
+        # Iterate the AR(2) model for each cascade level.
+        for i in xrange(num_cascade_levels):
+              for j in xrange(num_ens_members):
+                  R_c[j, i, :, :, :] = \
+                    autoregression.iterate_ar_model(R_c[j, i, :, :, :], PHI[i, :])
+        
+        # Compute the recomposed precipitation field from the cascade obtained 
+        # from the AR(2) model.
+        for j in xrange(num_ens_members):
+            R_r = [(R_c[j, i, -1, :, :] * sigma[i]) + mu[i] for i in xrange(num_cascade_levels)]
+            R_r = np.sum(np.stack(R_r), axis=0)
+            
+            # Advect the recomposed precipitation field to obtain the forecast for 
+            # time step t.
+            if add_perturbations:
+              V_ = V # TODO: Apply the motion perturbator here.
+            else:
+              V_ = V
+            
+            R_f_,D_ = advection.semilagrangian(R_r, V_, 1, D_prev=D[j], 
+                                               return_displacement=True)
+            D[j] = D_
+            R_f_ = R_f_[0]
+            
+            # Advect the precipitation mask and apply it to the output.
+            # TODO: Make the precipitation mask optional.
+            #MASK_p_ = advection.semilagrangian(MASK_p, V, 1, D_prev=D)
+            #R_f_[MASK_p < 0.5] = R_min
+            
+            R_f[j].append(R_f_)
+    
+    if num_ens_members == 1:
+        return np.stack(R_f[0])
+    else:
+        return np.stack([np.stack(R_f[j]) for j in xrange(num_ens_members)])
